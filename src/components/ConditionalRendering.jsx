20f6d2c3ecc161fc934c8641a08285d980879f5d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-08-23 05:37:04 +0800

    conditional rendering

--- src/components/ConditionalRendering.jsx
+++ src/components/ConditionalRendering.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
 const ConditionalRendering = () => {
-  const [course, setCourse] = useState("react");
+  const [course, setCourse] = useState("vue");
   return (
     <div>
       <p>Conditional Rendering </p>
-      {course === "react" ? <p>Thsi course Reac</p> : <p>This course Vue</p>}
+      {course === "react" ? (
+        <p>Thsi course Reac</p>
+      ) : course === "vue" ? (
+        <p>This course Vue</p>
+      ) : null}
     </div>
   );
 };
