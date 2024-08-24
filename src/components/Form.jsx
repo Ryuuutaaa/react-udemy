728609d284bffb2c45fcd761fd56e168794c8791
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-08-24 16:53:00 +0800

    text area input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,15 +2,27 @@ import { useState } from "react";
 
 const Form = () => {
   const [course, setCourse] = useState("");
+  const [desc, setDesc] = useState("");
+
   const hadnleChange = () => {
-    setCourse(event.target.value);
+    if (event.target.name === "course") {
+      setCourse(event.target.value);
+    } else if (event.target.name === "desc") {
+      setDesc(event.target.value);
+    }
   };
 
   console.log(course);
+  console.log(desc);
   return (
     <div>
       <form action="">
         <input name="course" value={course} onChange={hadnleChange} />
+        <textarea
+          name="desc"
+          id=""
+          value={desc}
+          onChange={hadnleChange}></textarea>
       </form>
     </div>
   );
