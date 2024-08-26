88e1a8f85eb6e6c9d8188d7af86332b99faa8d09
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-08-27 01:47:54 +0800

    how to create higher order component

---
+++ src/components/HOC/withCounter.jsx
@@ -0,0 +1,17 @@
+import { Component, useState } from "react";
+
+const withCounter = () => {
+  const newComponent = () => {
+    const [counter, setCounter] = useState(0);
+
+    const handleIncrement = () => {
+      setCounter((oldCounter) => oldCounter + 1);
+    };
+
+    return <Component counter={counter} handleIncrement={handleIncrement} />;
+  };
+
+  return newComponent;
+};
+
+export default withCounter;
