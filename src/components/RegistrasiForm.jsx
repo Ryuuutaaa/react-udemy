b77478e6636f23c8e9f852975a9e1e3ac9145157
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-08-27 01:20:09 +0800

    form register is done

--- src/components/RegistrasiForm.jsx
+++ src/components/RegistrasiForm.jsx
@@ -12,12 +12,18 @@ const RegistrasiForm = () => {
   });
 
   const handleChange = (e) => {
-    setData((olddata) => ({ ...olddata, [e.target.name]: e.target.value }));
+    if (e.target.type === "checkbox") {
+      setData((olddata) => ({ ...olddata, [e.target.name]: e.target.value }));
+    } else {
+      setData((olddata) => ({ ...olddata, [e.target.name]: e.target.value }));
+    }
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
 
-  console.log(data);
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -39,6 +45,69 @@ const RegistrasiForm = () => {
             value={data.email}
             onChange={handleChange}
           />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            onChange={handleChange}
+            value={data.confirmPassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Country</label>
+          <select name="country" value={data.country} onChange={handleChange}>
+            <option value="USA">USA</option>
+            <option value="Canada">Cadana</option>
+            <option value="UK">UK</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="">Male</label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            onChange={handleChange}
+          />
+          <br />
+          <label htmlFor="">Female</label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            onChange={handleChange}
+          />
+          <br />
+          <label htmlFor="">Other</label>
+          <input
+            type="radio"
+            name="gender"
+            value="other"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Aggrement</label>
+          <input
+            type="checkbox"
+            name="aggrement"
+            value={data.aggrement}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input type="submit" value="save" />
         </div>
       </form>
     </div>
