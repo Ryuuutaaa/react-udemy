3d851bb7538dbe37fad8f320f2c4cb6842f5e71d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-08-29 11:21:09 +0800

    create costum api

---
+++ src/components/CostumContex.jsx
@@ -0,0 +1,23 @@
+class Context {
+  constructor(value) {
+    this.value = value;
+  }
+
+  Provider = ({ children, value }) => {
+    this.value = value;
+    return children;
+  };
+
+  Consumer = ({ children }) => children.value;
+}
+
+function createContext(value = null) {
+  const contex = new Context(value);
+
+  return {
+    Provider: contex.Provider,
+    Consumer: contex.Consumer,
+  };
+}
+
+export default createContext;
