530a03c56291dc757d058cae146ad53d9b0849b8
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-09-26 01:02:22 +0800

    fetch api with axios

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Posts = () => {
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      console.log(response);
+    }
+    fetchPosts();
+  }, []);
   return (
     <div>
       <h2>Posts List</h2>
