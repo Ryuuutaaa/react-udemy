c232c64afeeceb0b93cbddc4f891bd8e24493394
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-09-26 01:29:25 +0800

    addjusetment

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+const Title = () => {
+  console.log("rendering title");
+  return (
+    <div>
+      <h2>This is title components</h2>
+    </div>
+  );
+};
+
+export default Title;
--- src/components/UseCallback.jsx
+++ src/components/UseCallback.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
+import Title from "./Title";
 
 const UseCallback = () => {
-  return <div>UseCallback</div>;
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  const handleIncrementByOne = () => {
+    setCounter1((oldCounter) => oldCounter + 1);
+  };
+
+  const handleIncrementByFive = () => {
+    setCounter2((oldCounter) => oldCounter + 5);
+  };
+
+  return (
+    <div>
+      <Title />
+    </div>
+  );
 };
 
 export default UseCallback;
