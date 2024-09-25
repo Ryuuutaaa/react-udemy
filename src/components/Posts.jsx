153276f086fe3fee8aaa1af7d70303ff5024a36f
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-09-26 01:05:19 +0800

    call data api

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,19 +1,28 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     async function fetchPosts() {
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );
-      console.log(response);
+      setPosts(response.data);
     }
     fetchPosts();
   }, []);
   return (
     <div>
       <h2>Posts List</h2>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 };
