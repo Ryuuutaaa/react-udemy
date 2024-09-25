89e4f1fb1e7fe073bc5eaa2caadecc3ed2495368
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-09-26 00:56:02 +0800

    add post jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
-import Timer from "./components/Timer";
-import CounterApp from "./components/CounterApp";
+import Posts from "./components/Posts";
 
 function App() {
-  const [toggle, setToggle] = useState(false);
-
   return (
     <>
       <div>
         {/* <courseContext.Provider value={{ course: "react" }}>
           <ComponentOne />
         </courseContext.Provider> */}
-        <button onClick={() => setToggle(!toggle)}>Click Toggle</button>
-        {toggle ? <CounterApp /> : <Timer />}
+        {/* <button onClick={() => setToggle(!toggle)}>Click Toggle</button>
+        {toggle ? <CounterApp /> : <Timer />} */}
+        <Posts />
       </div>
     </>
   );
---
+++ src/components/Posts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Posts = () => {
+  return (
+    <div>
+      <h2>Posts List</h2>
+    </div>
+  );
+};
+
+export default Posts;
