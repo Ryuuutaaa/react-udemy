05307b342d70f88b4e16aec854b575191c59e091
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-14 11:12:38 +0800

    feat : add useReff is done

--- src/components/UserReff.jsx
+++ src/components/UserReff.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const UserReff = () => {
   const [input, setInput] = useState({ name: "", email: "", password: "" });
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    console.log(inputRef.current);
+    inputRef.current.focus();
+    inputRef.current.style.border = "5px solid red";
+  }, []);
 
   const handleChange = (e) => {
     setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -10,6 +17,7 @@ const UserReff = () => {
     <div>
       <p>This useReff example</p>
       <input
+        ref={inputRef}
         type="text"
         name="nama"
         value={input.name}
