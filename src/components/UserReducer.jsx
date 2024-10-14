4092c1055261eb8ba48219a6996a0f2fb8381708
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-14 11:16:47 +0800

    feat : add reducer

---
+++ src/components/UserReducer.jsx
@@ -0,0 +1,13 @@
+import React, { useReducer } from "react";
+
+const UserReducer = () => {
+  return (
+    <div>
+      <button>+</button>
+      <p>Counter - 0</p>
+      <button>-</button>
+    </div>
+  );
+};
+
+export default UserReducer;
