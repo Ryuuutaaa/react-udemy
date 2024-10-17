7274ac2345c68ce8cb2f6c5c4b440fa4fb0a59cc
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-17 16:36:26 +0800

    feat : add postlist jsx

--- src/components/pages/PostList.jsx
+++ src/components/pages/PostList.jsx
@@ -1,15 +1,21 @@
 import { useSearchParams } from "react-router-dom";
 
-
-
 const PostList = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const filterValue = searchParams.get("filter");
+  console.log(filterValue);
+
   return (
     <div>
+      <h2>{filterValue === "latest" ? "Latest post list" : "all post list"}</h2>
       <h1>Post List</h1>
       <p>Post List 1</p>
       <p>Post List 2</p>
       <p>Post List 3</p>
+      <button onClick={() => setSearchParams({ filter: "latest" })}>
+        Latest post
+      </button>
+      <button onClick={() => setSearchParams({})}>Reset</button>
     </div>
   );
 };
