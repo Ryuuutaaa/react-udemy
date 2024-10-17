6b782f8c999dbc95902f932f4613049b78b4135a
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-17 12:17:52 +0800

    feat : add place order in home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("order-confirmation");
+  };
+
+  return (
+    <div>
+      <h1>this home </h1>
+      <button onClick={handleNavigate}>Place order this</button>
+    </div>
+  );
 };
 
 export default Home;
