dbcd636828bfe0d38cfb8a5520817aac1a69fdeb
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-17 12:27:15 +0800

    feat : add postlist

---
+++ src/components/pages/PostList.jsx
@@ -0,0 +1,12 @@
+const PostList = () => {
+  return (
+    <div>
+      <h1>Post List</h1>
+      <p>Post List 1</p>
+      <p>Post List 2</p>
+      <p>Post List 3</p>
+    </div>
+  );
+};
+
+export default PostList;
