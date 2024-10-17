49ce53eaf559d350174d770ef0204255c5d232bb
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-17 12:20:06 +0800

    feat : add button back to home and navigate programmatically is done

--- src/components/pages/OrderConfirmation.jsx
+++ src/components/pages/OrderConfirmation.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const OrderConfirmation = () => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
   return (
     <div>
       <h2>OrderConfirmation</h2>
+      <button onClick={handleBack}>Back to home</button>
     </div>
   );
 };
