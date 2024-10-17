606d01b965cb1025ec9068b23a85abfe5cea51b4
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-17 12:03:08 +0800

    feat : done navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
+    <nav>
       <Link to="/">Home</Link>
       <Link to="/about">About </Link>
       <Link to="/contact">Contact</Link>
-    </div>
+    </nav>
   );
 };
 
